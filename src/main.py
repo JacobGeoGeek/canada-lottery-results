8ecf8bb050be4c1bffa40be96a204d945ee5ae1a
Jacob Chaar <jchaar@k2geospatial.com>
2023-04-04 22:53:09 +0000

    add Cords middleware

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
 """"main.py"""
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .lottomax import lottomax_route
 
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(lottomax_route.router)
