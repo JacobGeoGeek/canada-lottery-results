a8d940b100b93278a9ef8f81d5fce6bd365bfdcb
Jacob Chaar <jchaar@k2geospatial.com>
2023-09-10 21:41:01 -0400

    remove unused class

--- src/lottomax/entities/winner.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-from .location import Location
-
-class Winner(BaseModel):
-    """Number of winner for each Match"""
-    total: int = Field(..., alias="total")
-    location: list[Location] = Field(..., alias="location")
