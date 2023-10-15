239712024fdd93052b9c62d9436a64adc0c83ab5
Jacob Chaar <jchaar@k2geospatial.com>
2023-10-15 17:58:34 -0400

    format the file

--- src/security/security_service.py
+++ src/security/security_service.py
@@ -6,4 +6,3 @@ def validate_rapidapi_proxy_secret(x_rapid_api_proxy_secret: Annotated[str | Non
     """Validate the RapidAPI Proxy Secret"""
     if configuration.environnement == Environnement.PROD  and x_rapid_api_proxy_secret != configuration.rapidapi_proxy_secret:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid X-RapidAPI-Proxy-Secret header")
-    
