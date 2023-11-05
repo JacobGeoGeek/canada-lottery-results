d6e8523d79fae0d3aca5a399e1dba4c945fdc508
Jacob Chaar <jchaar@k2geospatial.com>
2023-11-05 16:27:57 -0500

    Create functions for route and service to fetch 6/49 results by year

--- src/six_fourty_nine/six_fourty_nine_route.py
+++ src/six_fourty_nine/six_fourty_nine_route.py
@@ -1,6 +1,9 @@
 
-from fastapi import APIRouter
-from .six_fourty_nine_service import fing_all_years
+from typing import Final
+from fastapi import APIRouter, Path
+
+from .entities.result import Result
+from .six_fourty_nine_service import fing_all_years, find_649_results_by_year
 
 router = APIRouter(
     prefix="/6-49",
@@ -8,7 +11,20 @@ router = APIRouter(
     responses={404: {"description": "Not Found"}, 500: {"description": "Internal server error"}}
 )
 
+_649_YEARS: Final[list[int]] = fing_all_years()
+_649_START_YEAR: Final[int] = min(_649_YEARS)
+_649_LAST_YEAR: Final[int] = max(_649_YEARS)
+
 @router.get("/years", response_model=list[int])
 async def get_six_fourty_nine_years() -> list[int]:
     """Get all years from lotto 6/49"""
-    return fing_all_years()
+    return _649_YEARS
+
+@router.get("/years/{year}", response_model=list[Result])
+async def get_six_fourty_nine_result_by_year(year: int = Path(
+        title="The year of lotto 6/49 results to get",
+        ge=_649_START_YEAR,
+        le=_649_LAST_YEAR
+)):
+    """Get the lotto 6/49 numbers result by year"""
+    return find_649_results_by_year(year)
