1be42c4a8ce5bbdaa4cefbf531d0b2ea9620ee3b
Jacob Chaar <jacobchaar@live.ca>
2024-04-14 12:46:59 -0400

    Refactor daily_grand_factory.py and six_fourty_nine_factory.py

--- src/daily_grand/daily_grand_factory.py
+++ src/daily_grand/daily_grand_factory.py
@@ -1,5 +1,5 @@
 import datetime
-from json import loads
+from json import loads, dumps
 from typing import Final
 
 from sqlalchemy import Column
@@ -32,6 +32,36 @@ def build_daily_grand_prize_breakdown(data: DailyGrandResults) -> PrizeBreakdown
     bonuses_breakdown: Final[DetailBreakDown | None] = DetailBreakDown(**loads(data.bonus_breakdown)) if data.bonus_breakdown else None 
 
     return PrizeBreakdown(mainBreakdown=main_breakdown, bonusesBreakdown=bonuses_breakdown)
+
+def build_daily_grand_new_result(number_result: Result, prize_breakdown: PrizeBreakdown) -> DailyGrandResults:
+    """Build daily grand new result"""
+    return DailyGrandResults(
+        date=number_result.date,
+        game_id=2,
+        numbers=number_result.numbers,
+        grand_number=number_result.grand_number,
+        bonuses_draw=list(map(lambda bonus_draw: bonus_draw.model_dump_json(), number_result.bonuses_draw)),
+        prize=number_result.prize,
+        main_breakdown=prize_breakdown.main_breakdown.model_dump_json(),
+        bonus_breakdown=prize_breakdown.bonuses_breakdown.model_dump_json() if prize_breakdown.bonuses_breakdown else None
+    )
+
+def build_daily_grand_body_email(data: DailyGrandResults) -> str:
+    """Build daily grand body email"""
+    json_indent: Final[int] = 4
+    return f"""
+    <h1>Daily Grand Result</h1>
+    <p>Date: {data.date}</p>
+    <p>Numbers: {data.numbers}</p>
+    <p>Grand Number: {data.grand_number}</p>
+    <p>Prize: {data.prize}</p>
+    <p>Main Breakdown:</p>
+    <pre>{dumps(data.main_breakdown, indent=json_indent)}</pre>
+    <p>Bonus Breakdown:</p>
+    <pre>{dumps(data.bonus_breakdown, indent=json_indent)}</pre>
+    <p>Bonuses Draw:</p>
+    <pre>{dumps(data.bonuses_draw, indent=json_indent)}</pre>
+    """
 
 def _build_bonuses_draw(data: Column) -> list[BonusDraw]:
     """Build bonuses draw"""
--- src/six_fourty_nine/six_fourty_nine_factory.py
+++ src/six_fourty_nine/six_fourty_nine_factory.py
@@ -1,5 +1,5 @@
 import datetime
-from json import loads
+from json import loads, dumps
 from typing import Final
 
 from sqlalchemy import Column
@@ -15,9 +15,6 @@ from .models.prize_breakdown import PrizeBreakdown
 
 from .entities.six_fourty_nine_results import SixFourtyNineResults
 
-
-
-# ...
 def build_649_results(data: list[SixFourtyNineResults]) -> list[Result]:
   """Build 6/49 results"""
   results: list[Result] = []
@@ -35,7 +32,6 @@ def build_649_results(data: list[SixFourtyNineResults]) -> list[Result]:
       gold_ball_dict: Final[dict] = loads(value.gold_ball)
       gold_ball = GoldBall(number=gold_ball_dict["number"], prize=gold_ball_dict["prize"], isGoldBallDrawn=gold_ball_dict["is_gold_ball_drawn"])
 
-
     results.append(Result(date=date, classic=classic, guaranteed=guaranteed, goldBall=gold_ball))
 
   return results
@@ -46,6 +42,34 @@ def build_649_prize_breakdown(data: SixFourtyNineResults) -> PrizeBreakdown:
     numbers_matched: Final[list[NumbersMatched]] = _build_match_numbers(data.number_matched)
     return PrizeBreakdown(summary=summary, numbers_matched=numbers_matched)
 
+def build_649_new_result(number_result: Result, prize_breakdown: PrizeBreakdown) -> SixFourtyNineResults:
+   return SixFourtyNineResults(
+    date=number_result.date,
+    game_id=3,
+    classic=number_result.classic.model_dump_json(),
+    guaranteed=list(map(lambda guaranteed: guaranteed.model_dump_json(), number_result.guaranteed)) if number_result.guaranteed else None,
+    gold_ball=number_result.gold_ball.model_dump_json() if number_result.gold_ball else None,
+    summary=prize_breakdown.summary.model_dump_json() if prize_breakdown.summary else None,
+    number_matched=list(map(lambda number_matched: number_matched.model_dump_json(), prize_breakdown.numbers_matched)) if prize_breakdown.numbers_matched else None
+  )
+
+def build_649_body_email(data: SixFourtyNineResults) -> str:
+    """Build 6/49 body email"""
+    json_indent: Final[int] = 4
+    return f"""
+    <h1>6/49 Result</h1>
+    <p>Date: {data.date}</p>
+    <p>Classic:</p>
+    <pre>{dumps(data.classic, indent=json_indent)}</pre>
+    <p>Guaranteed:</p>
+    <pre>{dumps(data.guaranteed, indent=json_indent)}</pre>
+    <p>Gold Ball:</p>
+    <pre>{dumps(data.gold_ball, indent=json_indent)}</pre>
+    <p>Summary:</p>
+    <pre>{dumps(data.summary, indent=json_indent)}</pre>
+    <p>Numbers Matched:</p>
+    <pre>{dumps(data.number_matched, indent=json_indent)}</pre>
+    """
 
 def _build_summary(summary: Column) -> Summary:
     """Build summary"""
@@ -67,4 +91,5 @@ def _build_guaranteed(guaranteed: Column) -> list[Guaranteed]:
         guaranteed_dict = loads(value)
         result.append(Guaranteed(**guaranteed_dict))
     return result
+
 
