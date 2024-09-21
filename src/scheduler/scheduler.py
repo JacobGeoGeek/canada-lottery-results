cece8b04510dfb9c9c90a8be7c30a2e1fa2ffdc2
Jacob Chaar <jacobchaar@live.ca>
2024-09-21 13:49:48 -0400

    Refactor lottomax_service.py and lottomax_factory.py

--- src/scheduler/scheduler.py
+++ src/scheduler/scheduler.py
@@ -3,7 +3,7 @@ from typing import Final
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from src.daily_grand.daily_grand_service import insert_new_daily_grand_result
-from src.lottomax.lottomax_service import insert_new_lotto_result
+from src.lottomax.lottomax_service import insert_new_lotto_max_result
 from src.six_fourty_nine.six_fourty_nine_service import insert_new_649_result
 
 class Scheduler:
@@ -17,7 +17,7 @@ class Scheduler:
   def _insert_latest_lotto_max_result(self):
     date_yesterday: Final[datetime.date] = datetime.date.today() - datetime.timedelta(days=1)
     print(f"insert latest lotto max result {date_yesterday}")
-    insert_new_lotto_result(date_yesterday)
+    insert_new_lotto_max_result(date_yesterday)
   
   def _insert_latest_649_result(self):
     date_yesterday: Final[datetime.date] = datetime.date.today() - datetime.timedelta(days=1)
